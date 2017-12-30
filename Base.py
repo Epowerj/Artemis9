e7d5a8ad85b1098fcbaca3ce4fca3f689d0869fd
Epowerj <epowerj@gmail.com>
2017-12-29 21:52:29 -0500

    Base now saves owner name

--- Base.py
+++ Base.py
@@ -1,15 +1,17 @@
 
 class Base:
     name = str()
+    ownername = str()
     ownerid = int()
 
     units = []
     buildings = []
 
-    def __init__(self, userid, name):
+    def __init__(self, userid, ownername, name):
         self.ownerid = userid
+        self.ownername = ownername
         self.name = name
 
     def get_status(self):
         return "Base " + self.name + "\n" \
-                                     "Owner " + str(self.ownerid) + "\n"
+                                     "Owner " + self.ownername + "\n"
